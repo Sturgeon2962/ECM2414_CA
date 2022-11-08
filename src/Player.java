7a10ac99cdeb3387fd3d1be387919b80f97e83d8
Sturgeon2962 <tomasturgeon@gmail.com>
2022-11-08 14:35:46 +0000

    player attributes and get/set

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,27 @@
+import java.lang.reflect.Array;
+
 public class Player {
+
+    public String name;
+    public Integer Number;
+    private Array hand;
     
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getNumber() {
+        return Number;
+    }
+    public void setNumber(Integer number) {
+        Number = number;
+    }
+    public Array getHand() {
+        return hand;
+    }
+    public void setHand(Array hand) {
+        this.hand = hand;
+    }
 }
