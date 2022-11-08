b7144b49a8c5524fadfe316585709586cfc2d64e
Sturgeon2962 <tomasturgeon@gmail.com>
2022-11-08 13:50:09 +0000

    hand over to josh to run since need to install

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+public class Card {
+    /**
+     * attributes for the card class
+     */
+    private int denomination;
+
+    /**
+     * @return
+     */
+    public synchronized int getDenomination() {
+        return denomination;
+    }
+
+    /**
+     * @param denomination
+     */
+    public void setDenomination(int denomination) {
+        this.denomination = denomination;
+    }
+
+    /**
+     * Constructor to create a card object
+     * @param value
+     */
+    public Card(int value){
+        setDenomination(value);
+    }
+
+}
