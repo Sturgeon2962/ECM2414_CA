678ce36d2fcb003bd0551e072253af2bfc1ea03b
Sturgeon2962 <tomasturgeon@gmail.com>
2022-11-08 14:06:57 +0000

    removing redundent tests

--- src/tests/TestCard.java
+++ src/tests/TestCard.java
@@ -6,40 +6,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestCard {
-    
     import org.junit.experimental.categories.Category;
 
-    /**
-     *
-     * @author yw433
-     * This test class (together with LargestTest3) is 
-     * written to facilitate your 
-     * understanding of Categories in suites (SlowTestSuite)
-     */
-
-    @Category({SlowTests.class, FastTests.class})
-    public class LargestTest4 {
-        
-        public LargestTest4() {
-        }
-        
-        @Test
-        public void testOrder () {
-        assertEquals (9, Largest.largest (new int[] {8 ,9 ,7}));
-            assertEquals (9, Largest.largest (new int [] {8 ,9 ,7}));
-            assertEquals (9, Largest.largest (new int [] {7 ,8 ,9}));
-
-        }
-        
-        @Test
-        public void testEmpty () {
-            try {
-                Largest.largest(new int [] {});
-                fail (" Should have thrown an exception ");
-            } catch (RuntimeException e) {
-                assertTrue ( true );
-            }
-        }
-        
-    }
 }
